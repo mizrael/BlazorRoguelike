498529b6241767e56d5d18bb65e54c89450fd5d9
David Guida <david.guida.03@gmail.com>
2021-04-19 17:22:54 -0400

    first commit

---
+++ src/BlazorRoguelike.Web/Game/Components/GroundItemComponent.cs
@@ -0,0 +1,19 @@
+﻿using BlazorRoguelike.Core;
+using BlazorRoguelike.Core.Components;
+using BlazorRoguelike.Web.Game.Mechanics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRoguelike.Web.Game.Components
+{
+    public class GroundItemComponent : Component
+    {
+        private GroundItemComponent(GameObject owner) : base(owner)
+        {
+        }
+
+        public Item Item { get; set; }
+    }
+}
---
+++ src/BlazorRoguelike.Web/Game/Mechanics/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorRoguelike.Web.Game.Mechanics
+{
+    public record Item
+    {
+        public Item(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+    }
+}
